afbb85d402a66c0da07f1ecbc3a514a479b360fb
Rafael Ferreira Gomes <faelfg@gmail.com>
2026-08-26 00:02:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mosquitto Broker Service
+
+## How to install and run
+
+Simple run the command on the project root folder:
+
+`docker-compose up -d`
---
+++ build/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+if [ -z "$MQTT_USER" ] || [ -z "$MQTT_PASSWORD" ]; then
+    echo "ERRO: As variáveis MQTT_USER e MQTT_PASSWORD precisam ser definidas."
+    exit 1
+fi
+
+echo "Configurando usuário e senha do MQTT..."
+
+mosquitto_passwd -b -c /mosquitto/config/passwd "$MQTT_USER" "$MQTT_PASSWORD"
+
+exec "$@"
